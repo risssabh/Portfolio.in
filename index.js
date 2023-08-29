34c113010ea5955b43e58b68e70279b7ce4f7fa5
Rishabh Kumar Singh <104349949+risssabh@users.noreply.github.com>
2023-08-30 04:11:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+let typed = new Typed('.auto-input',{
+    strings: ['Full-Stack Developer.', 'Freelancer.', 'B.Tech COE Student.', 'Full-Stack Developer.'],
+    typeSpeed: 50,
+    backSpeed: 10,
+    backDelay: 2000,
+    loop: true,
+})
